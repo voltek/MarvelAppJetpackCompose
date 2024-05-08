3c072c36a4c1d0fc447148bdb83603c718edfda9
Alan Hernández <involting@gmail.com>
2024-05-07 22:07:19 -0600

    Minor: Fixed the HeroDetailScreen counter

--- app/src/main/java/com/voltek/marvelapp/feature_marvel_heroes/presentation/hero_detail/components/HeroDetailScreen.kt
+++ app/src/main/java/com/voltek/marvelapp/feature_marvel_heroes/presentation/hero_detail/components/HeroDetailScreen.kt
@@ -6,16 +6,41 @@
 
 package com.voltek.marvelapp.feature_marvel_heroes.presentation.hero_detail.components
 
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -39,7 +64,12 @@ import com.voltek.marvelapp.R
 import com.voltek.marvelapp.core.presentation.components.texts.AutoSizeText
 import com.voltek.marvelapp.core.presentation.components.texts.AutoSizeTextModel
 import com.voltek.marvelapp.core.util.Constants
-import com.voltek.marvelapp.feature_marvel_heroes.domain.model.characters.*
+import com.voltek.marvelapp.feature_marvel_heroes.domain.model.characters.ComicsModel
+import com.voltek.marvelapp.feature_marvel_heroes.domain.model.characters.EventsModel
+import com.voltek.marvelapp.feature_marvel_heroes.domain.model.characters.ResultModel
+import com.voltek.marvelapp.feature_marvel_heroes.domain.model.characters.SeriesModel
+import com.voltek.marvelapp.feature_marvel_heroes.domain.model.characters.StoriesModel
+import com.voltek.marvelapp.feature_marvel_heroes.domain.model.characters.ThumbnailModel
 import com.voltek.marvelapp.feature_marvel_heroes.presentation.hero_detail.HeroDetailViewModel
 import com.voltek.marvelapp.feature_marvel_heroes.presentation.util.PresentationUtils.LANDSCAPE_AMAZING
 import com.voltek.marvelapp.feature_marvel_heroes.presentation.util.PresentationUtils.getImageUrl
@@ -214,7 +244,8 @@ fun CharacterDetailStatsSection(
             items(characterStats) {
                 CharacterStatElement(
                     iconResId = it.iconResId,
-                    statTitle = stringResource(it.statTitleResId)
+                    statTitle = stringResource(it.statTitleResId),
+                    statValue = it.statValue
                 )
             }
         }
@@ -227,13 +258,14 @@ fun CharacterDetailStatsSection(
 fun CharacterStatElement(
     modifier: Modifier = Modifier,
     iconResId: Int,
-    statTitle: String
+    statTitle: String,
+    statValue: Int = 0
 ) {
 
-    val stat by remember { mutableStateOf(0) }
+    val stat by remember { mutableStateOf(statValue) }
 
     val statCounter by animateIntAsState(
-        targetValue = stat,
+        targetValue = statValue,
         animationSpec = tween(
             durationMillis = 1000,
             easing = FastOutSlowInEasing
